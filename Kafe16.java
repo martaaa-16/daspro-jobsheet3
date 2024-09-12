73f56a03f518af2ee36fc54bf4dc92d3c0b6871f
martaaa-16 <martaprama@gmail.com>
2024-09-12 09:59:29 +0700

    percobaan 2

--- Kafe16.java
+++ Kafe16.java
@@ -6,7 +6,7 @@ public class Kafe16 {
         Scanner input = new Scanner(System.in);
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
-        double hargaKopi = 13000.0, hargaTeh = 8000.0, hargaRoti = 15000.0, totalHarga, nominalBayar;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
         float diskon = 10 / 100f;
 
         System.out.print("Masukkan keanggotaan (true/false): ");
@@ -21,7 +21,7 @@ public class Kafe16 {
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
         
         nominalBayar = totalHarga - (diskon * totalHarga);
-
+       
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " + nominalBayar);
