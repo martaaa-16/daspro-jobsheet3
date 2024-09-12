f8f50c29e35e9978d8fcccafa30dc85a853cdd68
martaaa-16 <martaprama@gmail.com>
2024-09-12 08:52:16 +0700

    Modifikasi kode program

--- Siakad16.java
+++ Siakad16.java
@@ -7,7 +7,7 @@ public class Siakad16 {
         String nama, nim;
         char kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
@@ -21,15 +21,18 @@ public class Siakad16 {
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiAkhir = (nilaiKuis * 0.20 + nilaiTugas * 0.15 + nilaiUTS * 0.30 + nilaiUAS * 0.35) / 4;
 
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("Kelas:  " + kelas);
         System.out.println("Absen: " + absen);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
+        
     }
 }
